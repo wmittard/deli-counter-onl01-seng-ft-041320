0102477dc2f386cfc616df91fe9691d4aab4d41d
William Mittard <billymittard@ymail.com>
2020-04-15 15:36:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,9 @@ def line(x)
   if x.length == 0 
     puts "The line is currently empty." 
   else
+    x.each.with_index(1) do |name, index|
+      line_array.push ("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join("")}"
+  end
+end
