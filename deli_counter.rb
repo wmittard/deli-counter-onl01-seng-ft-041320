dcc7541f64d0da31cb05dff9a5102ade4cd54d7a
William Mittard <billymittard@ymail.com>
2020-04-15 15:38:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,20 +2,6 @@ katz_deli = []
 
 def line(x)
   line_array = []
-  if x.length == 0 
-    puts "The line is currently empty." 
-  else
-    x.each.with_index(1) do |name, index|
-      line_array.push ("#{index}. #{name}")
-    end
-    puts "The line is currently: #{line_array.join("")}"
-  end
-end 
-
-katz_deli = []
-
-def line(x)
-  line_array = []
   if x.length == 0
     puts "The line is currently empty."
   else
